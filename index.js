3b0a30a75b0f71b4e14238122ad4e78171fb3412
Ryan <ryanmchenry215@gmail.com>
2018-12-02 16:46:20 -0600

    added support for client ID as environment variable

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js');
 const magicConch = require('magic-conch-core');
 
-if (!process.env.DISCORD_API_KEY) {
+if (!process.env.DISCORD_BOT_TOKEN || !process.env.DISCORD_CLIENT_ID) {
   require('dotenv').config();
 }
 
-const token = process.env.DISCORD_API_KEY;
+const token = process.env.DISCORD_BOT_TOKEN;
+const clientID = process.env.DISCORD_CLIENT_ID;
 
 const bot = new Discord.Client();
 
@@ -14,13 +15,12 @@ bot.login(token)
    console.log(e);
  });
 
-
 bot.on('ready', () => {
   console.log('Ready!');
 });
 
 bot.on('message', (msg) => {
-  if (msg.isMentioned('507355928950210584')) {
+  if (msg.isMentioned(clientID)) {
     msg.reply(magicConch(msg.content));
   }
 });
