d017da783d3e470e4a2379d639061fb3a46f064e
Ryan <ryanmchenry215@gmail.com>
2019-02-17 18:05:46 -0600

    Fixed bug that caused bot to not respond to 'was' questions correctly

--- utils/typeCheckers.js
+++ utils/typeCheckers.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   isYesOrNo: q => {
-    return /^<.*>\W*(?:will|do|did|am|are|were|could|can|should|have|has|may|might|must|ought|shall).*/gim.test(q);
+    return /^<.*>\W*(?:will|do|did|am|are|were|could|can|should|have|has|may|might|must|ought|shall|was).*/gim.test(q);
   },
 
   isEitherOr: q => {
