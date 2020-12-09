fe719c5e5a2becb2376de80622956aea657d75ba
Ryan <ryan@pop-os.localdomain>
2020-12-09 17:32:06 -0500

    Fixed folder structure and imports

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const magicConch = require('magic-conch-core');
+const magicConch = require('./lib/magicConch');
 
 const token = process.env.DISCORD_BOT_TOKEN;
 const clientID = process.env.DISCORD_CLIENT_ID;
@@ -7,9 +7,9 @@ const clientID = process.env.DISCORD_CLIENT_ID;
 const bot = new Discord.Client();
 
 bot.login(token)
- .catch(e => {
-   console.log(e);
- });
+  .catch(e => {
+    console.log(e);
+  });
 
 bot.on('ready', () => {
   console.log('Ready!');
